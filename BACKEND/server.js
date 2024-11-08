2edfbcd9720ca9e0eb68b6840ec14f59c4befcd1
ayush jangid <ayushjangid2103@gmail.com>
2024-11-08 22:02:09 +0530

    trailblaze backend

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -11,16 +11,14 @@ const PORT = process.env.PORT || 8080; // Use port 8080 or fallback to 7000
 
 connectDB();
 app.use(express.json());
+app.use(morgan('dev')); // Optional: Add morgan middleware for logging requests
 
 app.use("/", rootroute);
 app.use("/api/treks", trekroute); // Assuming you want to use trekroute as well
 
-// app.listen(PORT, () => {
-//     console.log(`Server is running on http://localhost:${PORT}`.bgBlack.white);
-// }
-// );
-// mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => console.log("Database connected"))
-//   .catch((err) => console.error("Database connection error:", err));
+// Listen on the specified port
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`.bgBlack.white);
+});
 
 module.exports = app;
